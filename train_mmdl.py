a969f0648a9de1918ad6597aa76887de05f5c481
sophiajeng <jengs@ohsu.edu>
2024-07-12 09:12:39 -0700

    Add files via upload
    Added local reweigh

--- train_mmdl.py
+++ train_mmdl.py
@@ -24,12 +24,15 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 
+from torch.func import functional_call, vmap, grad
+
+
 if torch.cuda.is_available():
     device = torch.device("cuda")
 else:
     device = torch.device("cpu")
 
-def process_probs (model_output, names, labels, p_dict):
+def process_probs (model_output, names, labels, p_dict,pt_wts):
     probability = F.softmax(model_output, dim=1).data.squeeze()    
     probs = probability.cpu().numpy()
     for i in range(labels.size(0)):
@@ -39,7 +42,8 @@ def process_probs (model_output, names, labels, p_dict):
                 'prob_0': 0, 
                 'prob_1': 0,
                 'label': labels[i].item(),      
-                'img_num': 0}
+                'img_num': 0,
+                'weight':pt_wts[p]}
         if probs.ndim == 2:
             p_dict[p]['prob_0'] += probs[i, 0]
             p_dict[p]['prob_1'] += probs[i, 1]
@@ -48,6 +52,21 @@ def process_probs (model_output, names, labels, p_dict):
             p_dict[p]['prob_0'] += probs[0]
             p_dict[p]['prob_1'] += probs[1]
             p_dict[p]['img_num'] += 1
+
+def process_loss (model_loss, names,p_loss,pt_wts):
+    loss = model_loss.detach().cpu().numpy()
+    for i in range(loss.size):
+        l = names[i]
+        if l not in p_loss.keys(): 
+          p_loss[l] = {
+            "img_loss" : pt_wts[l]*loss[i],
+            "img_num" : 0}
+        if loss.ndim == 2:
+            p_loss[l]["img_loss"] += pt_wts[l]*loss[i,0]
+            p_loss[l]["img_num"] += 1
+        else:
+            p_loss[l]["img_loss"] += pt_wts[l]*loss[i]
+            p_loss[l]["img_num"] += 1            
 
 def tabulate_probs (p_dict):
     y_true = []
@@ -65,7 +84,7 @@ def tabulate_probs (p_dict):
         if p_dict[key]['label'] == predict:
             correct += 1
         y_true.append(p_dict[key]['label'])
-        score_list.append([p_dict[key]['prob_0']/p_dict[key]["img_num"],p_dict[key]['prob_1']/p_dict[key]["img_num"]])
+        score_list.append([(p_dict[key]['prob_0']/p_dict[key]["img_num"]),(p_dict[key]['prob_1']/p_dict[key]["img_num"])])
         y_pred.append(predict)
         
     score_list = pd.DataFrame(score_list)
@@ -75,9 +94,38 @@ def tabulate_probs (p_dict):
 
     return test_acc, y_true, y_pred, preid_list, score_list
 
+def tabulate_loss (p_loss):
+    loss_list = {}
+    for key in p_loss.keys():
+        loss_list[key] = p_loss[key]['img_loss']/p_loss[key]["img_num"]
+    #loss_list = pd.DataFrame(loss_list)
+    return pd.DataFrame(loss_list,index=[0])
 
-def train_model(model_ft, train_file, clin_features, clin_pts, num_epochs=10,
+
+def compute_grad(model,sample, clinic, target,loss_fn):
+    sample = sample.unsqueeze(0)  # prepend batch dimension for processing
+    #clinic = clinic.unsqueeze(0)
+    target = target.unsqueeze(0)
+
+    prediction = model(sample,clinic)
+    loss = loss_fn(prediction, target)
+    grad_list = []
+    for l in loss:
+      grad_list[l]=torch.autograd.grad(l,list(model.parameters()),allow_unused=True)
+    return grad_list
+
+
+def compute_sample_grads(model,data, clinic,targets,loss_fn,bs):
+    """ manually process each sample with per sample gradient """
+    sample_grads = [compute_grad(model,data[i], clinic,targets[i],loss_fn) for i in range(bs)]
+    sample_grads = zip(*sample_grads)
+    sample_grads = [torch.stack(shards) for shards in sample_grads]
+    return sample_grads
+
+
+def train_model(model_ft, train_file, clin_features, clin_pts, pt_wts,num_epochs=10,
                 batch_size = 64, num_workers = 1, lr = 0.001, momentum = 0.9, step_size=7, gamma=0.1):
+
 
     data_transforms = transforms.Compose([
         transforms.Resize(224),
@@ -92,10 +140,10 @@ def train_model(model_ft, train_file, clin_features, clin_pts, num_epochs=10,
     )
 
     model_ft.to(device)
-
+    #print(pt_wts.keys())
     model_ft.train()
- 
-    criterion = nn.CrossEntropyLoss()  
+    #added reduction='none' so we have per sample loss 
+    criterion = nn.CrossEntropyLoss(reduction='none')  
     # Observe that all parameters are being optimized
     optimizer = optim.SGD(model_ft.parameters(), lr=lr, momentum=momentum)  
     # Decay LR by a factor of 0.1 every 7 epochs    
@@ -109,11 +157,14 @@ def train_model(model_ft, train_file, clin_features, clin_pts, num_epochs=10,
     for epoch in range(num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))  
 
-        running_loss = 0.0
+        #running_loss = 0.0
+        running_loss = torch.zeros(1,dtype=torch.float64)
+        running_loss = running_loss.to(device)
         running_corrects = 0
         total = 0
 
         person_prob_dict = dict()
+        person_loss_dict = dict()
 
         # Iterate over data
         for inputs_, labels_, names_, _ in trainloader:
@@ -128,42 +179,56 @@ def train_model(model_ft, train_file, clin_features, clin_pts, num_epochs=10,
             with torch.set_grad_enabled(True):
                
                 X_train_minmax = torch.from_numpy(np.array([clin_features[:,clin_pts.index(i)] for i in names_], dtype=np.float32))
+                batch_pt_wts = [pt_wts[k] for k in names_]
+                batch_pt_wts = torch.from_numpy(np.array(list(batch_pt_wts)))
+                batch_pt_wts = batch_pt_wts.to(device)
+                #print(batch_pt_wts)
                 outputs_ = model_ft(inputs_, X_train_minmax.to(device))
 
-                process_probs(outputs_, names_, labels_, person_prob_dict)
+                process_probs(outputs_, names_, labels_, person_prob_dict,pt_wts)
 
                 _, preds = torch.max(outputs_, 1)
+                #loss with be per sample loss 
                 loss = criterion(outputs_, labels_)
-
+                #per_sample_grads = compute_sample_grads(model_ft,inputs_, X_train_minmax.to(device), labels_,criterioni,batch_size,names_)
+                process_loss(loss, names_,person_loss_dict,pt_wts)
+                #get mean of loss
+                loss = loss * batch_pt_wts
                 # backward + optimize only if in training phase
-                loss.backward()
+                #loss.backward()
+                loss.mean().backward()
                 optimizer.step()
 
             # statistics
-            running_loss += loss.item() * inputs_.size(0)
+            #running_loss += loss.item() * inputs_.size(0)
+            loss = loss.to(device)
+            #running_loss += loss * inputs_.size(0)
+            running_loss = torch.cat((running_loss,loss))
             running_corrects += torch.sum((preds == labels_.data).int())
             
             total += preds.size(0)
 
-        epoch_loss = running_loss / total
+        #epoch_loss = running_loss / total
+        epoch_loss = running_loss
         epoch_acc = running_corrects / total
 
         epoch_acc_sample, *_ = tabulate_probs(person_prob_dict)
-
+        epoch_loss_sample = tabulate_loss(person_loss_dict)
         scheduler.step()
         if epoch_acc > best_acc:
             best_acc = epoch_acc
             best_model_wts = copy.deepcopy(model_ft.state_dict())
 
-        print('{} Loss: {:.4f} Per Image Acc: {:.4f} Per Sample Acc: {:.4f}'.format(
-            'train', epoch_loss, epoch_acc, epoch_acc_sample))
-
+        #print('{} Loss: {:.4f} Per Image Acc: {:.4f} Per Sample Acc: {:.4f}'.format(
+        print('{} Loss: {} Per Image Acc: {:.4f} Per Sample Acc: {:.4f}'.format(
+#            'train', epoch_loss, epoch_acc, epoch_acc_sample))
+            'train', epoch_loss_sample, epoch_acc, epoch_acc_sample))
     # load best model weights
     model_ft.load_state_dict(best_model_wts)
     
     return model_ft, best_acc, steps
 
-def test_model (model, test_file, clin_features, clin_pts, batch_size = 64, num_workers = 1):
+def test_model (model, test_file, clin_features, clin_pts, clin_wts,batch_size = 64, num_workers = 1):
 
     #test-specific transforms
     data_transforms = transforms.Compose([
@@ -186,7 +251,7 @@ def test_model (model, test_file, clin_features, clin_pts, batch_size = 64, num_
             images, labels, names_, images_names = data
             X_test_minmax = [clin_features[:,clin_pts.index(i)] for i in names_]
             outputs = model(images.to(device), torch.from_numpy(np.array(X_test_minmax, dtype=np.float32)).to(device))
-            process_probs(outputs, names_, labels, person_prob_dict)
+            process_probs(outputs, names_, labels, person_prob_dict,pt_wts)
             
     test_acc, y_true, y_pred, preid_list, score_list = tabulate_probs(person_prob_dict)
 
@@ -198,11 +263,14 @@ def preprocess_clinical (clinical_file):
     #note this is computing the min-max transform over all the data
     clin_dt=pd.read_csv(clinical_file)  
     clin_pts=[i for i in clin_dt.TCGA_ID]
-    raw_clin_features=[clin_dt[i] for i in clin_dt.columns[1:]]      
+    clin_wts=[i for i in clin_dt.weight]
+    raw_clin_features=[clin_dt[i] for i in clin_dt.columns[1:(clin_dt.shape[1]-1)]]      
     min_max_scaler = preprocessing.MinMaxScaler()  
     clin_features = min_max_scaler.fit_transform(raw_clin_features)
-
-    return clin_features, clin_pts
+    pt_wts = {}
+    for key in range(len(clin_pts)):
+       pt_wts[clin_pts[key]] = clin_wts[key]
+    return clin_features, clin_pts, pt_wts
 
 if __name__ == "__main__":
 
@@ -216,14 +284,14 @@ if __name__ == "__main__":
 
     Path(args.out_dir).mkdir(parents=True, exist_ok=True)
 
-    clin_features, clin_pts = preprocess_clinical(args.clinical_file)
+    clin_features, clin_pts, pt_wts= preprocess_clinical(args.clinical_file)
 
     since = time.time()
 
     #can tweak model here if needed 
     model = Cnn_With_Clinical_Net(clin_features.shape[0]) 
 
-    model_trn, train_acc, steps = train_model(model, args.train_file, clin_features, clin_pts, num_epochs=args.n_epochs)
+    model_trn, train_acc, steps = train_model(model, args.train_file, clin_features, clin_pts, pt_wts, num_epochs=args.n_epochs)
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}h {:.0f}m'.format(
@@ -236,7 +304,7 @@ if __name__ == "__main__":
 
     print('Starting testing...')
 
-    test_acc, y_true, y_pred, preid_list, score_list = test_model (model_trn, args.test_file, clin_features, clin_pts)
+    test_acc, y_true, y_pred, preid_list, score_list = test_model (model_trn, args.test_file, clin_features, clin_pts, pt_wts)
 
     test_basename = os.path.basename(args.test_file)
 
